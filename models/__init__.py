60d839ad5c9a834ce17bfd3e1ee7b396ac1ebd7f
Liam Chu <113557299+notLiamChu@users.noreply.github.com>
2026-09-09 13:34:11 -0700

    Add checkpointing functions to models module

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,12 @@
 from models.loader import load_paligemma_model
 from models.inspector import inspect_paligemma_architecture, ModelProfile, print_architecture_report
+from models.checkpointing import save_sliced_paligemma, load_sliced_paligemma
 
 __all__ = [
     "load_paligemma_model",
     "inspect_paligemma_architecture",
     "ModelProfile",
     "print_architecture_report",
+    "save_sliced_paligemma",
+    "load_sliced_paligemma",
 ]
