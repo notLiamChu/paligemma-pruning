5ff4d8babab114f5329d60b6ae480a0af2ab4a2b
Liam Chu <113557299+notLiamChu@users.noreply.github.com>
2026-09-09 14:09:19 -0700

    Document paligemma-pruning experiments in __init__.py
    Add docstring for paligemma-pruning experiment suites

---
+++ experiments/__init__.py
@@ -0,0 +1,6 @@
+"""
+paligemma-pruning experiment suites:
+- exp1: SigLIP vision tower channel pruning
+- exp2: Gemma 2 language decoder SwiGLU pruning
+- exp3: Sequential joint vision-language compression
+"""
